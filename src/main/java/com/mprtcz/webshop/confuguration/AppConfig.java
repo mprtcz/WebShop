dba5a80eb94dbf71263917ecaee1dc0d69b8eaef
Mordin <azet89@gmail.com>
2016-08-27 22:39:43 +0200

    changed spring framework version in pom file

--- src/main/java/com/mprtcz/webshop/confuguration/AppConfig.java
+++ src/main/java/com/mprtcz/webshop/confuguration/AppConfig.java
@@ -1,5 +1,6 @@
 package com.mprtcz.webshop.confuguration;
 
+import com.mprtcz.webshop.converter.RoleToUserProfileConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
@@ -7,10 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.format.FormatterRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
